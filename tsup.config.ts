722c2b02b8dfabd909c69b1d9ba52630b2148a92
catorch <catorch8@outlook.com>
2025-07-12 14:50:52 -0500

    Update tsup config - disable sourcemaps for production builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig([
+  // TypeScript declarations only
+  {
+    entry: ["src/index.ts", "src/core.ts", "src/locale/*.ts", "src/cli.ts"],
+    dts: { only: true },
+    outDir: "dist",
+  },
+  // JavaScript builds
   {
     entry: ["src/index.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     clean: true,
     minify: true,
     sourcemap: false,
@@ -13,7 +19,6 @@ export default defineConfig([
   {
     entry: ["src/core.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -21,7 +26,6 @@ export default defineConfig([
   {
     entry: ["src/locale/de.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -29,7 +33,6 @@ export default defineConfig([
   {
     entry: ["src/locale/fr.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -37,16 +40,13 @@ export default defineConfig([
   {
     entry: ["src/locale/hi.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
   },
-
   {
     entry: ["src/locale/ar.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -54,7 +54,6 @@ export default defineConfig([
   {
     entry: ["src/locale/zh.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -62,7 +61,6 @@ export default defineConfig([
   {
     entry: ["src/locale/sv.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -70,7 +68,6 @@ export default defineConfig([
   {
     entry: ["src/locale/ru.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -78,7 +75,6 @@ export default defineConfig([
   {
     entry: ["src/locale/emoji.ts"],
     format: ["esm", "cjs"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
@@ -86,7 +82,6 @@ export default defineConfig([
   {
     entry: ["src/cli.ts"],
     format: ["esm"],
-    dts: true,
     minify: true,
     sourcemap: false,
     outDir: "dist",
